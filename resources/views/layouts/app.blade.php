78877a10f0668c5f764a2132868a83b42290b448
taito.kobayashi <taito.a.kobayashi@rakuten.com>
2018-05-28 04:31:56 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>MessageBoard</title>
+        <title>TaskList</title>
     </head>
 
     <body>
+        @include('commons.error_tasks')
         @yield('content')
     </body>
 </html>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,6 +4,7 @@
 
 <h1>メッセージ新規作成ページ</h1>
 
+
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
         {!! Form::label('content', 'メッセージ:') !!}
