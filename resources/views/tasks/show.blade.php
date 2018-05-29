8d203fa80ac112f43e93a85c1872affaed283fa1
taito.kobayashi <taito.a.kobayashi@rakuten.com>
2018-05-29 05:51:08 +0000

    bootstrap design

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,15 +2,28 @@
 
 @section('content')
 
-<h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+<h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>タイトル: {{ $task->status }}</p>
-    <p>{{ $task->content }}</p>
+    <p>ステータス: {{ $task->status }}</p>
+    <p><table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table></p>
 
-     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
+     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
